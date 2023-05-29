ec7b4451486e38c005266d5182944f741a180332
Zafinaaa <zafina13a.work@gmail.com>
2023-05-29 20:37:52 +0700

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    defaultCommandTimeout: 80000,
+    pageLoadTimeout: 80000
+  },
+});
---
+++ cypress/support/LUMA/createAccountPage.js
@@ -0,0 +1,18 @@
+class createAccountPage {
+    firstNameTxt = '#firstname'
+    lastNameTxt = '#lastname'
+    emailTxt = '#email_address'
+    passwordTxt = '#password'
+    confrimPasswordTxt = '#password-confirmation'
+    createAnAccountBtn = '#form-validate > .actions-toolbar > div.primary > .action'
+    passwordStrengthMeter ='#password-strength-meter'
+    passwordStrengthMeterLabel = '#password-strength-meter-label'
+    //error msg section
+    firstNameRequiredMsg ='#firstname-error'
+    lastNameRequiredMsg = '#lastname-error'
+    emailRequiredMsg ='#email_address-error'
+    passwordErrorMsg = '#password-error'
+    confrimPasswordErrorMsg = '#password-confirmation-error'
+
+}
+export default createAccountPage
